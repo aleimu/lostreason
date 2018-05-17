e132c121a57b01275b6e9d556ef2d7f5b757f402
Langj <2086530297@qq.com>
2018-05-17 14:55:03 +0800

    Create 参考.sh

---
+++ Hook/参考.sh
@@ -0,0 +1,20 @@
+先来看看Hooking Android App 的Hook关键点实现Hook模块的注意点：
+实现 IXposedHookLoadPackage接口
+确定要Hook的Android App的包名
+判断要Hook的包名
+确定要Hook的AndroidApp的方法
+findAndHookMethod 语法
+
+https://blog.csdn.net/nicolelili1/article/details/52471820
+https://blog.csdn.net/swimmer2000/article/details/52384817
+https://blog.csdn.net/dzsw0117/article/details/79292608
+
+https://blog.csdn.net/true_maitian/article/details/54922611
+https://blog.csdn.net/eastmoon502136/article/details/50596806
+https://blog.csdn.net/kmyhy/article/details/54929109
+
+https://github.com/duqian291902259/WechatHook-Dusan
+https://github.com/rovo89/XposedBridge/wiki/Development-tutorial
+https://github.com/rovo89/XposedExamples/tree/master/RedClock
+https://github.com/rovo89/XposedBridge
+https://github.com/Gh0u1L5/WechatSpellbook
